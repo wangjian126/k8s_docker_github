35f31fe7552dbfb94889b46b33e0a25566313601
wangjian8157 <747064257@qq.com>
2020-11-06 14:19:14 +0800

    删除无用文件

--- src/main/java/com/demo/sk8/k8s_docker_github/controller/Demo1Controller.java
+++
@@ -1,6 +0,0 @@
-package com.demo.sk8.k8s_docker_github.controller;
-
-public class Demo1Controller {
-
-	//测试
-}
--- src/main/java/com/demo/sk8/k8s_docker_github/controller/ee.java
+++
@@ -1,4 +0,0 @@
-package com.demo.sk8.k8s_docker_github.controller;
-
-public class ee {
-}
