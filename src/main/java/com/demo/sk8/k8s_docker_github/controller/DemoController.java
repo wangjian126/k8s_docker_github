48a1697b0b4183c5dd3358e77e0919af297e1df1
wangjian8157 <747064257@qq.com>
2020-11-06 11:12:39 +0800

    测试

--- src/main/java/com/demo/sk8/k8s_docker_github/controller/DemoController.java
+++ src/main/java/com/demo/sk8/k8s_docker_github/controller/DemoController.java
@@ -8,9 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
-	/**
-	 * 获取参数配置列表
-	 */
+
 	@GetMapping("/getInfo")
 	public String getInfo()
 	{
